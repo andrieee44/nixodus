3982dc0b45e6c02c47621e4ee841caeb868a8745
Kris Andrie Ortega <andrieee44@gmail.com>
2026-07-08 12:49:43 +0800

    Add help message for nixodus CLI

--- cmd/nixodus/main.go
+++ cmd/nixodus/main.go
@@ -13,7 +13,7 @@ import (
 var mainNix string
 
 func run() error {
-	type nixArgs struct {
+	type options struct {
 		Nixpkgs     string
 		Nixodus     string
 		NixAppImage string
@@ -23,42 +23,57 @@ func run() error {
 	}
 
 	var (
-		args     nixArgs
-		argsFile *os.File
-		cmd      exec.Cmd
+		opts     options
+		optsFile *os.File
+		cmd      *exec.Cmd
 		err      error
 	)
 
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Usage: %[1]s [OPTION]... <PACKAGE>...
+Bundle multiple Nix PACKAGE(S) into a single multicall binary
+
+Examples:
+  %[1]s hello dos2unix
+  %[1]s --system riscv64-linux hello
+  echo '{"packages": ["hello"]}' | %[1]s --json
+
+Flags:
+`, os.Args[0])
+
+		flag.PrintDefaults()
+	}
+
 	flag.StringVar(
-		&args.Nixpkgs,
+		&opts.Nixpkgs,
 		"nixpkgs",
 		"github:NixOS/nixpkgs/nixos-26.05",
 		"nixpkgs flake reference",
 	)
 
 	flag.StringVar(
-		&args.Nixodus,
+		&opts.Nixodus,
 		"nixodus",
 		"github:andrieee44/nixodus",
 		"nixodus flake reference",
 	)
 
 	flag.StringVar(
-		&args.NixAppImage,
+		&opts.NixAppImage,
 		"nix-appimage",
 		"github:ralismark/nix-appimage",
 		"nix-appimage flake reference",
 	)
 
 	flag.StringVar(
-		&args.System,
+		&opts.System,
 		"system",
 		"CURRENT",
 		`Target platform e.g. "x86_64-linux"`,
 	)
 
 	flag.BoolVar(
-		&args.useJSON,
+		&opts.useJSON,
 		"json",
 		false,
 		"Read JSON from stdin",
@@ -66,29 +81,29 @@ func run() error {
 
 	flag.Parse()
 
-	argsFile, err = os.CreateTemp(os.TempDir(), "nixodus-*.json")
+	optsFile, err = os.CreateTemp(os.TempDir(), "nixodus-*.json")
 	if err != nil {
 		return err
 	}
-	defer os.Remove(argsFile.Name())
-	defer argsFile.Close()
+	defer os.Remove(optsFile.Name())
+	defer optsFile.Close()
 
-	args.Packages = flag.Args()
-	if args.useJSON {
-		err = json.NewDecoder(os.Stdin).Decode(&args.Packages)
+	opts.Packages = flag.Args()
+	if opts.useJSON {
+		err = json.NewDecoder(os.Stdin).Decode(&opts.Packages)
 		if err != nil {
 			return err
 		}
 	}
 
-	err = json.NewEncoder(argsFile).Encode(args)
+	err = json.NewEncoder(optsFile).Encode(opts)
 	if err != nil {
 		return err
 	}
 
-	cmd = *exec.Command(
+	cmd = exec.Command(
 		"nix", "build", "--impure", "--no-link", "--print-out-paths",
-		"--argstr", "argsFile", argsFile.Name(),
+		"--argstr", "argsFile", optsFile.Name(),
 		"--expr", mainNix,
 	)
 	cmd.Stdout = os.Stdout
